23fd820b6107cc0678e96f3e2cf5ffb8ee8c35ea
meetantariksh <meetantariksh@gmail.com>
2017-06-17 15:28:54 +0530

    Update

--- src/app/props/serverProps.ts
+++ src/app/props/serverProps.ts
@@ -1,5 +1,5 @@
 //AWS Server
-export const backend="ec2-13-126-38-241.ap-south-1.compute.amazonaws.com/";
+export const backend="http://ec2-13-126-38-241.ap-south-1.compute.amazonaws.com/";
 
 //Local Server
 //export const backend="http://localhost:3500/";
